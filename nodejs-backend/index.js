aacee3ab5fb3528586a811228c9489ebdbc4f490
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-24 00:47:57 +0300

    fix : bug in connecting to the mongo db

---
+++ nodejs-backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const DB_CONNECT = process.env.DB_CONNECT || "";
+const PORT = process.env.API_PORT || "";
+
+mongoose.connect(
+  DB_CONNECT,
+  () => console.log('connected to db')
+);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log('Server running'));
