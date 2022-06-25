640a99a1776a7e7cbf5e9fc16e051a4d8f47dc8d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-25 16:13:02 +0300

    build : function to check if jwt token exists

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,23 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+     //Checking if the token exists
+     const checkToken =  () => {
+      try {
+        let token = localStorage.getItem("token");
+        if ( token )
+            return true;
+        else
+            return false; 
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<></>
   );
 }
 
