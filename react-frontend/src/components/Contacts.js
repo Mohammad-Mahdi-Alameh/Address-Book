f420858498709b83d067028f764073f818691cc2
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-26 18:51:20 +0300

    build : function to filter the contacts by first name and in 3 modes ; starts with,endswith,include

--- react-frontend/src/components/Contacts.js
+++ react-frontend/src/components/Contacts.js
@@ -1,16 +1,46 @@
 import React from "react";
 import Contact from "./Contact";
+import { useState } from "react";
+import Button from "./Button";
 const Contacts = ({ contacts }) => {
- 
-    return(<> {contacts.map((contact) => (
-      <Contact
-        key={contact._id}
-        contact={contact}
-        // getInfo={getInfo}
-        />
-    ))}
+  // const [filter, setFilter] = useState('All');
+  // const FILTER_MAP = {
+  //   All: () => true,
+  //   ByName: contact => contact.last_name.startsWith('t2'),
+  // };
+  // const FILTER_NAMES = Object.keys(FILTER_MAP);
+  // const filterList = FILTER_NAMES.map(name => (
+  //   <Button key={name} text={name}/>
+  // ));
+function filterFirstName_start(v){
+  return(
+  contacts.filter(contact => contact.last_name.startsWith(v)).map(filteredContact => (
+    <Contact
+    key={filteredContact._id}
+    contact={filteredContact}
+    />)));
+}
+function filterFirstName_end(v){
+  return(
+  contacts.filter(contact => contact.last_name.endsWith(v)).map(filteredContact => (
+    <Contact
+    key={filteredContact._id}
+    contact={filteredContact}
+    />)));
+}
+function filterFirstName_includes(v){
+  return(
+  contacts.filter(contact => contact.last_name.includes(v)).map(filteredContact => (
+    <Contact
+    key={filteredContact._id}
+    contact={filteredContact}
+    />)));
+}
+    return(<> 
+     {filterFirstName_includes("t2")}
+    
+  
       </>
 );
 }
-
   export default Contacts;
